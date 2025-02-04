eeabc690763d3e223000768cc6d13c577793bf92
EngineerBR90 <reinaldo.vfarias@gmail.com>
2025-02-04 15:43:23 -0300

    Update AppPC2.py

--- AppPC2.py
+++ AppPC2.py
@@ -36,7 +36,8 @@ def calcular_perda_carga(vazao, diametro, comprimento, comp_equivalente, materia
         resultado_texto += f"""
         \n### Recomendações de segurança:
         - Adicione +20% como margem de segurança: {perda_total * 1.20:.3f} mca
-        - Adicione ao cálculo a altura geométrica característica da instalação (entre o NA e a posição da MB)
+        - Adicione ao cálculo a altura geométrica característica da instalação 
+        - (entre o N.A. e a posição da MB)
         """
         return resultado_texto
     except ValueError:
